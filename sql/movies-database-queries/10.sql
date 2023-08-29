f87048ab1a5c797df5d54af73004c14bef26dd16
Fitz <108022185+FitzBrendan@users.noreply.github.com>
2023-08-29 14:24:43 -0400

    copy to public FitzBrendan Movies Queries 10.sql

---
+++ sql/movies-database-queries/10.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT     name
+FROM                people
+    JOIN    directors
+        ON      directors.person_id = people.id
+    JOIN    movies
+        ON      movies.id = directors.movie_id
+    JOIN    ratings
+        ON      ratings.movie_id = movies.id
+WHERE       ratings.rating >= 9.0;
