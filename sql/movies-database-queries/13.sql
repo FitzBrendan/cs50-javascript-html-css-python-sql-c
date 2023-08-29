0af9523ac6c22127b2544e34eef5547f35e4a8e2
Fitz <108022185+FitzBrendan@users.noreply.github.com>
2023-08-29 14:23:11 -0400

    copy to public FitzBrendan Movies Queries 13.sql

---
+++ sql/movies-database-queries/13.sql
@@ -0,0 +1,10 @@
+SELECT name FROM people
+    JOIN stars ON stars.person_id=people.id
+        JOIN movies ON stars.movie_id=movies.id
+WHERE movies.id IN
+(SELECT movies.id FROM movies
+    JOIN people ON stars.person_id=people.id
+        JOIN stars ON stars.movie_id=movies.id
+WHERE people.name="Kevin Bacon"
+AND people.birth=1958)
+AND people.name != "Kevin Bacon";
