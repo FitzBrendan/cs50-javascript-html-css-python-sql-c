12f50724a26a35ac337ef71dd4169fecce50871e
Fitz <108022185+FitzBrendan@users.noreply.github.com>
2023-08-29 13:25:22 -0400

    copy to public FitzBrendan volume-control.c

---
+++ c/volume-control.c
@@ -0,0 +1,52 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE]; // buffer to read and write header
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t samples; // buffer to read and write samples
+    while (fread(&samples, sizeof(int16_t), 1, input))
+    {
+        // fread will return 0 (break out of loop) at end of file with nothing to read
+        samples *= factor;
+        fwrite(&samples, sizeof(int16_t), 1, output);
+    }
+    // Close files
+    fclose(input);
+    fclose(output);
+}
